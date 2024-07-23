f1c3eb24adb10a9d3c089a3278ee183f1e5f0899
matan kalifa <matandev6556@gmail.com>
2024-07-23 17:26:01 +0300

    add yonatan code

--- src/models/Admin.ts
+++
@@ -1,9 +0,0 @@
-// src/models/Admin.ts
-
-import User, { UserType } from './User';
-
-export default class Admin extends User {
-  constructor(uid: string, name: string, email: string, type: UserType) {
-    super(uid, name, email, 'Admin');
-  }
-}
